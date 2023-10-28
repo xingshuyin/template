90f991c388aacffbdfcaa099dfa706142ecb5fd9
xingshuyin <xingshuyin@outlook.com>
2023-10-28 11:22:40 +0800

    通过cookie自动刷新token

--- back-faskapi/login_cookie.py
+++ back-faskapi/login_cookie.py
@@ -18,62 +18,97 @@ class Login(BaseModel):
     captcha: str
 
 
-async def parse_token(token: str):
-    try:
-        token_data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-    except jwt.exceptions.ExpiredSignatureError:
+@app.middleware("http")
+async def refresh_token(request: Request, call_next):
+    """
+    刷新token
+    """
+    response = await call_next(request)
+    if request.url.path == '/token/':
+        return response
+    response = await parse_token(request, response)
+    return response
+
+
+async def parse_token(request: Request, response: Response):
+    """
+    解析token
+    """
+    token = request.cookies.get("token")
+    refresh = request.cookies.get("refresh")
+    client = await get_client(request)
+    token_data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'], options={'verify_exp': False})
+    refresh_data = jwt.decode(refresh, SECRET_KEY, algorithms=['HS256'], options={'verify_exp': False})
+    if token_data['client'] != client:
+        raise HTTPException(status_code=401, detail='令牌无效')
+
+    time_now = datetime_.now().timestamp()
+    if float(token_data['exp']) > time_now:
+        return response
+    elif float(token_data['exp']) < time_now and float(refresh_data['exp']) > time_now:
+        token, refresh = await make_token(request, refresh_data['user'])
+        response.set_cookie(key='token', value=token, httponly=True)
+        response.set_cookie(key='refresh', value=refresh, httponly=True)
+    else:
         raise HTTPException(status_code=401, detail='令牌过期')
-    return token_data
+    return response
+
+
+async def make_token(request: Request, user_id):
+    """
+    生成token
+    """
+    time_now = datetime_.now()
+    client = await get_client(request)
+    token = jwt.encode({
+        'user': user_id,
+        'exp': datetime_.timestamp(time_now + timedelta(seconds=5)),
+        'iat': datetime_.timestamp(time_now),
+        'client': client
+    }, SECRET_KEY, algorithm='HS256')
+    refresh = jwt.encode({
+        'user': user_id,
+        'exp': datetime_.timestamp(time_now + timedelta(seconds=10)),
+        'iat': datetime_.timestamp(time_now),
+        'client': client
+    }, SECRET_KEY, algorithm='HS256')
+    return token, refresh
 
 
 async def encode_password(password: str):
+    """
+    加密密码
+    """
     return hashlib.sha256(password.encode()).hexdigest()
+
+
+async def get_client(request: Request) -> str:
+    """
+    获取客户端标识
+    """
+    return hashlib.md5((request.client.host + request.headers.get('user-agent')).encode()).hexdigest()
 
 
 @app.post("/token/", status_code=200)
 async def token(request: Request, data: Login, response: Response):
+    """
+    登录/获取token
+    """
     user_obj = await user.all().filter(username=data.username)
     if len(user_obj) == 0:
         response.status_code = 401
         return {'detail': '账号或密码错误'}
     user_obj = user_obj[0]
     if (user_obj.password == await encode_password(data.password)):
-        time_now = datetime_.now()
-        token = jwt.encode({
-            'user': user_obj.id,
-            'exp': datetime_.timestamp(time_now + timedelta(seconds=5)),
-            'iat': datetime_.timestamp(time_now),
-            'is_super': user_obj.is_super,
-        }, SECRET_KEY, algorithm='HS256')
-        refresh = jwt.encode({
-            'user': user_obj.id,
-            'exp': datetime_.timestamp(time_now + timedelta(seconds=5)),
-            'iat': datetime_.timestamp(time_now),
-            'is_super': user_obj.is_super,
-        }, SECRET_KEY, algorithm='HS256')
+
+        token, refresh = await make_token(request, user_obj.id)
+
         response.set_cookie(key='token', value=token, httponly=True)
         response.set_cookie(key='refresh', value=refresh, httponly=True)
-        return {'token': token, 'refresh': refresh}
+        return {'detail': '登录成功'}
     else:
         response.status_code = 401
         return {'detail': '账号或密码错误'}
-
-
-@app.post("/refresh/", status_code=200)
-async def refresh(request: Request, response: Response, refresh: Annotated[str | None, Cookie()] = None):
-    """
-    Generate token for user
-    """
-    token_data = await parse_token(refresh)
-    time_now = datetime_.now()
-    token = jwt.encode({
-        'user': token_data['user'],
-        'exp': datetime_.timestamp(time_now + timedelta(seconds=5)),
-        'iat': datetime_.timestamp(time_now),
-        'is_super': token_data['is_super'],
-    }, SECRET_KEY, algorithm='HS256')
-    response.set_cookie(key="token", value=token, httponly=True)
-    return token
 
 
 @app.post("/create_user/", status_code=201)
